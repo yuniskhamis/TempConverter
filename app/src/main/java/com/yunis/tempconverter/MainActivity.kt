c062af1588daa6b207fcab5e5a19966b2bc5428a
yuniskhamis <yuniskhamis3@gmail.com>
2021-07-19 14:54:32 -0400

    Changed TextView to EditText and added control flow so app would not crash when button is clicked

--- app/src/main/java/com/yunis/tempconverter/MainActivity.kt
+++ app/src/main/java/com/yunis/tempconverter/MainActivity.kt
@@ -3,35 +3,35 @@ package com.yunis.tempconverter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val celsius: TextView = findViewById(R.id.tempCelsius)
+        val celsius: EditText= findViewById(R.id.tempCelsius)
 
 
-        val resultFahrenheit: TextView = findViewById(R.id.tempFahrenheit)
+        val resultFahrenheit: EditText = findViewById(R.id.tempFahrenheit)
 
         val convertButton: Button = findViewById(R.id.button)
         val resetButton: Button = findViewById(R.id.button2)
        convertButton.setOnClickListener{
 
-
-            val fahrenheitResult: Float = (celsius.text.toString().toFloat() * 9 / 5) + 32
-            resultFahrenheit.text = fahrenheitResult.toString()
-
-
-            val celsiusResult: Float = ((resultFahrenheit.text.toString().toFloat()) - 32) * 5/9
-            celsius.text = celsiusResult.toString()
+if (resultFahrenheit.text.toString().isEmpty()) {
+    val fahrenheitResult: Float = (celsius.text.toString().toFloat() * 9 / 5) + 32
+    resultFahrenheit.setText(fahrenheitResult.toString())
+} else {
+    val celsiusResult: Float = (resultFahrenheit.text.toString().toFloat() - 32) * 5 / 9
+    celsius.setText(celsiusResult.toString())
+}
 
 
         }
 
         resetButton.setOnClickListener {
-            celsius.text = ""
-            resultFahrenheit.text = ""
+            celsius.setText("")
+            resultFahrenheit.setText("")
         }
 
     }
